c3b8fa978dcfaca96056d047a025c97be4fe5d50
guido <70071968+punkedwerewolf@users.noreply.github.com>
2022-04-15 15:58:14 -0700

    ohmigod

--- public/bloom.js
+++
@@ -1,7 +0,0 @@
- $(function(){
-      $("#header").load("/header.html");
-      $("#nav").load("/navbar.html"); 
-      $("#left").load("/left.html");
-      $("#right").load("/right.html");
-      $("#footer").load("/footer.html");
-    });
---
+++ public/scr/lyt.js
@@ -0,0 +1,7 @@
+$(function(){
+	$("#hed").load("/hed.html");
+	$("#nvw").load("/nav.html"); 
+	$("#lft").load("/lft.html");
+	$("#rgt").load("/rgt.html");
+	$("#fot").load("/fot.html");
+});
