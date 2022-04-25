9d621953aedac31d7799d5ff1d772e67d1081cf3
guido <70071968+punkedwerewolf@users.noreply.github.com>
2022-04-25 12:40:52 -0700

    f

---
+++ public/scr/blg.js
@@ -0,0 +1,1 @@
+$('.prv').click(function () {var cur = $('.pan').index($('.pan.tiv'));if (cur!=0) {$('.pan').removeClass('tiv');$('.pan').eq(cur-1).addClass('tiv');}});$('.nxt').click(function () {var cur = $('.pan').index($('.pan.tiv'));if (cur!=$('.pan').length-1) {$('.pan').removeClass('tiv');$('.pan').eq(cur+1).addClass('tiv');}});
