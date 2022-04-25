a5c69d47a6e05f3c37921ef1d022df8acbda2f7c
guido <70071968+punkedwerewolf@users.noreply.github.com>
2022-04-25 12:43:40 -0700

    Update blg.js

--- public/scr/blg.js
+++ public/scr/blg.js
@@ -1,1 +1,14 @@
-$('.prv').click(function () {var cur = $('.pan').index($('.pan.tiv'));if (cur!=0) {$('.pan').removeClass('tiv');$('.pan').eq(cur-1).addClass('tiv');}});$('.nxt').click(function () {var cur = $('.pan').index($('.pan.tiv'));if (cur!=$('.pan').length-1) {$('.pan').removeClass('tiv');$('.pan').eq(cur+1).addClass('tiv');}});
+$('.prv').click(function () {
+    var cur = $('.pan').index($('.pan.tiv'));
+    if (cur!=0) {
+        $('.pan').removeClass('tiv');
+        $('.pan').eq(cur-1).addClass('tiv');
+    }
+});
+$('.nxt').click(function () {
+    var cur = $('.pan').index($('.pan.tiv'));
+    if (cur!=$('.pan').length-1) {
+        $('.pan').removeClass('tiv');
+        $('.pan').eq(cur+1).addClass('tiv');
+    }
+});
